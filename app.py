6c8096c0778374058c68d9518862078d1d812b5c
PAVANMEESALA9 <124555056+PAVANMEESALA9@users.noreply.github.com>
2024-04-04 18:38:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Employee Salary Details", page_icon="💰", layout="centered")
+st.set_page_config(page_title="Employee Salary Details", page_icon="💰", layout="wide")
 
 # Load the CSV file
 @st.cache  # Add caching to improve app performance
@@ -10,21 +10,54 @@ def load_data():
 
 df = load_data()
 
-# Filter employees by name
-employee_name = st.selectbox('Select Employee Name', ['All'] + sorted(df['EmpName'].unique()))
+num_employees = df['EmpName'].nunique()
+num_citys=df['City'].nunique()
 
-# Filter DataFrame based on selected employee name
-filtered_df = df if employee_name == 'All' else df[df['EmpName'] == employee_name]
+# Count employees by city
+city_employee_counts = df.groupby('City').size()
 
-# Calculate the number of unique employees
-num_employees = filtered_df['EmpName'].nunique()
+st.title("Streamlit Example:")
+st.divider()
 
-# Display title and metric
-st.title("Streamlit Example")
-st.metric(label="No. of Employees", value=num_employees)
+left_column, middle_column =st.columns(2)
+# Add metric
+with left_column:
+    st.metric(label="No. of Employees", value=num_employees)
+with middle_column:   
+    st.metric(label="No. of Cities",value=num_citys)
+st.divider()
 
-# Display the bar chart
-st.bar_chart(filtered_df['Salary'].groupby(filtered_df['EmpName']).sum())
+# Display charts and toggle switches side by side
+col1, col2 = st.columns(2)
 
-# Display the table
-st.table(filtered_df)
+left_, Right_ =st.columns(2)
+
+#Selection of Employee
+st.sidebar.header("Filter Here:")
+EmpName = st.sidebar.multiselect(
+     "Select Employee:",
+     options=df["EmpName"].unique(),
+     default=df["EmpName"].unique()
+)
+df_selection= df.query("EmpName==@EmpName")
+
+# Display charts and toggle switches
+with left_:
+    st.subheader("Employee Salary")
+    chart_type_salary = st.radio("Select Chart Type", ["Bar Chart", "Line Chart"], key='chart_type_salary')
+    if chart_type_salary == "Bar Chart":
+        st.bar_chart(data=df, x='EmpName', y='Salary')
+    elif chart_type_salary == "Line Chart":
+        st.line_chart(data=df, x='EmpName', y='Salary')
+with Right_:
+    st.subheader("City Wise Employee Count")
+    chart_type_employee_count = st.radio("Select Chart Type", ["Line Chart", "Bar Chart"], key='chart_type_employee_count')
+    if chart_type_employee_count == "Line Chart":
+        st.line_chart(city_employee_counts)
+    elif chart_type_employee_count == "Bar Chart":
+        st.bar_chart(city_employee_counts)
+    
+
+# Display the entire DataFrame
+st.subheader("Employee Data")
+st.table(df_selection)
